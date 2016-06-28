8581e5cb72b764df5e24ed5036bce0809e4ca8d2
John Wingfield <john@johnwingfield.com>
2016-06-28 07:49:44 +0200

    Initial commit with file read/write

---
+++ src/com/johnwingfield/Tracker.java
@@ -0,0 +1,85 @@
+package com.johnwingfield;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Tracker extends JPanel implements ActionListener {
+	protected static JButton bLoad, bSave;
+	String file_name = "C:/Dropbox/Working/Tracker.txt";
+
+	public Tracker() {
+		bLoad = new JButton("Load log file");
+		bLoad.setActionCommand("load");
+		bLoad.addActionListener(this);
+
+		bSave = new JButton("Save log file");
+		bSave.setActionCommand("save");
+		bSave.addActionListener(this);
+		bSave.setEnabled(false);
+
+		add(bLoad);
+		add(bSave);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if ("load".equals(e.getActionCommand())) {
+			bLoad.setEnabled(false);
+			bSave.setEnabled(true);
+
+			try {
+				ReadFile file = new ReadFile(file_name);
+				String[] aryLines = file.OpenFile();
+
+//				for (int i = 0; i < aryLines.length; ++i)
+//					System.out.println(aryLines[i]);
+			}
+			catch (IOException ioE) { // var e of type IOException
+				System.out.println(ioE.getMessage());
+			}
+		}
+		else {
+			bLoad.setEnabled(true);
+			bSave.setEnabled(false);
+
+			try {
+				WriteFile data = new WriteFile(file_name, true);
+				data.WriteToFile("Another line of text");
+			}
+			catch (IOException ioE) {
+				System.out.println(ioE.getMessage());
+			}
+		}
+	}
+
+	// Create the GUI and show it.
+	private static void createGUI() {
+		JFrame.setDefaultLookAndFeelDecorated(true);
+
+		// Create and set up the frame.
+		JFrame frame = new JFrame("Tracker");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// Create and set up the content pane.
+		Tracker contentPane = new Tracker();
+		contentPane.setOpaque(true); // content panes must be opaque
+		frame.getRootPane().setDefaultButton(bLoad);
+		frame.setContentPane(contentPane);
+
+		// Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+
+			public void run() {
+				createGUI();
+			}
+		});
+	}
+}
---
+++ src/com/johnwingfield/WriteFile.java
@@ -0,0 +1,28 @@
+package com.johnwingfield;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+public class WriteFile {
+	private String path;
+	private boolean append_to_file = false;
+	
+	public WriteFile(String file_path) {
+		path = file_path;
+	}
+
+	// second constructor, will be used if append value is passed when object created
+	public WriteFile(String file_path, boolean append_value) {
+		path = file_path;
+		append_to_file = append_value;
+	}
+	
+	public void WriteToFile(String textLine) throws IOException {
+		FileWriter write = new FileWriter(path, append_to_file);
+		PrintWriter print_line = new PrintWriter(write);
+		
+		print_line.printf("%s" + "%n",textLine);
+		print_line.close();
+	}
+}
